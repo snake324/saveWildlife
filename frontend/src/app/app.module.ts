98595eea09123fe6248598dcf2f943a919846b4d
snake324 <kristi4n90@gmail.com>
2023-07-10 15:47:39 +0200

    mat-cards created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +26,8 @@ import { FamiliesComponent } from './characters/views/families/families.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/characters/components/card-families/card-families.component.ts
+++ frontend/src/app/characters/components/card-families/card-families.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-families.component.scss']
 })
 export class CardFamiliesComponent {
-
+  images = [
+    { url: '../../../../assets/images/tiger-4427000_1280 1 2.png', name: 'FÉLIDOS' },
+    { url: '../../../../assets/images/wolf-8089783_1280 1 2.png', name: 'CÁNIDOS' },
+    { url: '../../../../assets/images/gecko-2299365_1280 1 2.png', name: 'REPTILES' },
+    { url: '../../../../assets/images/snow-17854_1280 1 2.png', name: 'MUSTELIDAE' },
+    { url: '../../../../assets/images/rabbit-1903016_1280 1 2.png', name: 'LEPORIDAE' }
+  ];
 }
