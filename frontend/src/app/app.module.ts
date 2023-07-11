7cb5fb93fcf44dd68b9bfae178ede37887a6f396
TxemaBlanco <sam@example.comtxemabl@hotmail.com>
2023-07-11 16:21:16 +0200

    rectangle_name

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './global/header/header.component';
 import { FooterComponent } from './global/footer/footer.component';
 import { FamiliesComponent } from './characters/components/families/families.component';
-import { ViewsComponent } from './characters/views/views.component';
+
 import { ServiceComponent } from './characters/service/service.component';
 import { ModelsComponent } from './characters/models/models.component';
+import { AnimaldetailsComponent } from './characters/components/animaldetails/animaldetails.component';
+import { Animalsdetails2Component } from './characters/views/animalsdetails2/animalsdetails2.component';
 
 
 @NgModule({
@@ -17,9 +19,11 @@ import { ModelsComponent } from './characters/models/models.component';
     HeaderComponent,
     FooterComponent,
     FamiliesComponent,
-    ViewsComponent,
+
     ServiceComponent,
     ModelsComponent,
+    AnimaldetailsComponent,
+    Animalsdetails2Component,
 
   ],
   imports: [
