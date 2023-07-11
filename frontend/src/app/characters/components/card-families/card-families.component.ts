057f2ca425e19a31b15c0b6918d2604560ed17ee
snake324 <kristi4n90@gmail.com>
2023-07-11 11:59:12 +0200

    card-routing

--- frontend/src/app/characters/components/card-families/card-families.component.ts
+++ frontend/src/app/characters/components/card-families/card-families.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card-families',
@@ -7,10 +8,17 @@ import { Component } from '@angular/core';
 })
 export class CardFamiliesComponent {
   images = [
-    { url: '../../../../assets/images/tiger-4427000_1280 1 2.png', name: 'FÉLIDOS' },
-    { url: '../../../../assets/images/wolf-8089783_1280 1 2.png', name: 'CÁNIDOS' },
-    { url: '../../../../assets/images/gecko-2299365_1280 1 2.png', name: 'REPTILES' },
-    { url: '../../../../assets/images/snow-17854_1280 1 2.png', name: 'MUSTELIDAE' },
-    { url: '../../../../assets/images/rabbit-1903016_1280 1 2.png', name: 'LEPORIDAE' }
+    { url: '../../../../assets/images/tiger-4427000_1280 1 2.png', name: 'FÉLIDOS', route: ''},
+    { url: '../../../../assets/images/wolf-8089783_1280 1 2.png', name: 'CÁNIDOS', route: '' },
+    { url: '../../../../assets/images/gecko-2299365_1280 1 2.png', name: 'REPTILES', route: '' },
+    { url: '../../../../assets/images/snow-17854_1280 1 2.png', name: 'MUSTELIDAE', route: '' },
+    { url: '../../../../assets/images/rabbit-1903016_1280 1 2.png', name: 'LEPORIDAE', route: '' }
   ];
+
+  constructor(private router: Router) {}
+
+  navigateTo(route: string) {
+    this.router.navigateByUrl(route);
+  }
+
 }
