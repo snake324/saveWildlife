8e64fdae29be6ff3a149694b66c21e0ebe78e338
kayyra <anarodriguez.p@hotmail.es>
2023-07-12 12:50:38 +0200

    list-species added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './global/header/header.component';
@@ -11,6 +10,8 @@ import { ServiceComponent } from './characters/service/service.component';
 import { ModelsComponent } from './characters/models/models.component';
 import { AnimalsCounterComponent } from './characters/components/animals-counter/animals-counter.component';
 import { FamiliesComponent } from './characters/views/families/families.component';
+
+import { FilterPipe } from './characters/components/pipes/filter.pipe';
 import { ListSpeciesComponent } from './characters/components/list-species/list-species.component';
 
 
@@ -24,6 +25,7 @@ import { ListSpeciesComponent } from './characters/components/list-species/list-
     CardFamiliesComponent,
     AnimalsCounterComponent,
     FamiliesComponent,
+    FilterPipe,
     ListSpeciesComponent
   ],
   imports: [
