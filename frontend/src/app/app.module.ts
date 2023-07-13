1746fc34fa061818ca8256fc789488995fb14f5b
snake324 <kristi4n90@gmail.com>
2023-07-13 12:09:40 +0200

    api service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { FooterComponent } from './global/footer/footer.component';
 import { LoginComponent } from './global/login/login.component';
 import { PopuploginComponent } from './characters/components/popuplogin/popuplogin.component';
 import { CardFamiliesComponent } from './characters/components/card-families/card-families.component';
-import { ServiceComponent } from './characters/service/service.component';
 import { ModelsComponent } from './characters/models/models.component';
 import { AnimalsCounterComponent } from './characters/components/animals-counter/animals-counter.component';
 import { FamiliesComponent } from './characters/views/families/families.component';
@@ -25,7 +24,6 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     HeaderComponent,
     FooterComponent,
     FamiliesComponent,
-    ServiceComponent,
     LoginComponent,
     PopuploginComponent,
     ModelsComponent,
