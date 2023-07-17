14992cc7b2151df20647df9fc64a14b4f59197e2
snake324 <kristi4n90@gmail.com>
2023-07-17 16:57:06 +0200

    datasql updated

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO families (id_families,name) VALUES (DEFAULT,'FÉLIDOS');
-INSERT INTO families (id_families,name) VALUES (DEFAULT,'CÁNIDOS');
-INSERT INTO families (id_families,name) VALUES (DEFAULT,'REPTILES');
-INSERT INTO families (id_families,name) VALUES (DEFAULT,'MUSTELIDAE');
-INSERT INTO families (id_families,name) VALUES (DEFAULT,'LEPORIDAE');
+INSERT INTO families (id_family,name) VALUES (DEFAULT,'FÉLIDOS');
+INSERT INTO families (id_family,name) VALUES (DEFAULT,'CÁNIDOS');
+INSERT INTO families (id_family,name) VALUES (DEFAULT,'REPTILES');
+INSERT INTO families (id_family,name) VALUES (DEFAULT,'MUSTELIDAE');
+INSERT INTO families (id_family,name) VALUES (DEFAULT,'LEPORIDAE');
