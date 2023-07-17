9c082f21f502bceb47a21491520996b9bad6206c
snake324 <kristi4n90@gmail.com>
2023-07-17 15:43:25 +0200

    get functionally

--- backend/src/main/java/rinocerontes/de/java/save/wil/dlife/controllers/FamilyController.java
+++ backend/src/main/java/rinocerontes/de/java/save/wil/dlife/controllers/FamilyController.java
@@ -1,17 +1,21 @@
 package rinocerontes.de.java.save.wil.dlife.controllers;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import rinocerontes.de.java.save.wil.dlife.models.Family;
 import rinocerontes.de.java.save.wil.dlife.services.FamilyService;
 
 @RestController
-@RequestMapping(path = "&{api-endpoint}")
 public class FamilyController {
     
     FamilyService service;
@@ -31,5 +35,17 @@ public class FamilyController {
     public Family show(@PathVariable("id") Long id) {
         Family family = service.findById(id);
         return family;
+    }
+
+    @PostMapping(path = "/families")
+    public ResponseEntity<Family> store(@RequestBody Family family){
+        Family familySaved = service.save(family);
+        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(familySaved);
+    }
+
+    @DeleteMapping(path = "/families/{id}")
+    public Map<String, String> destroy(@PathVariable("id") Long id) {
+        Map<String, String> msg = service.delete(id);
+        return msg;
     }
 }
--- backend/src/main/java/rinocerontes/de/java/save/wil/dlife/services/FamilyService.java
+++ backend/src/main/java/rinocerontes/de/java/save/wil/dlife/services/FamilyService.java
@@ -1,8 +1,12 @@
 package rinocerontes.de.java.save.wil.dlife.services;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import rinocerontes.de.java.save.wil.dlife.models.Family;
 import rinocerontes.de.java.save.wil.dlife.repositories.FamilyRepository;
@@ -25,5 +29,24 @@ public class FamilyService {
         Family family = repository.findById(id).orElseThrow();
         return family;
     }
-    
+
+    public Family save(Family country) {
+
+        if (country.getName() == "") {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "name empty");
+        }
+
+        Family countrySaved = repository.save(country);
+        return countrySaved;
+    }
+
+    public Map<String, String> delete(long id){
+        Family family = repository.findById(id).orElseThrow();
+        repository.deleteById(family.getId());
+
+        Map<String, String> msg = new HashMap<>();
+        msg.put("message", "Family" + family.getName() + "successfully deleted!");
+
+        return msg;
+    }
 }
