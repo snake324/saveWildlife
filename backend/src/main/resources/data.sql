597b1d7c9db085e75e6a7a458d1884b15e06501f
snake324 <kristi4n90@gmail.com>
2023-07-17 11:39:07 +0200

    family controller, model, repository, service, data

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO families (id_families,name) VALUES (DEFAULT,'FÉLIDOS');
+INSERT INTO families (id_families,name) VALUES (DEFAULT,'CÁNIDOS');
+INSERT INTO families (id_families,name) VALUES (DEFAULT,'REPTILES');
+INSERT INTO families (id_families,name) VALUES (DEFAULT,'MUSTELIDAE');
+INSERT INTO families (id_families,name) VALUES (DEFAULT,'LEPORIDAE');
